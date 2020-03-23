b98523b5219063ca2dd3f29fba6a63d692097531
Paul Taylor <paul@nms.com.au>
2020-03-24 08:16:54 +1100

    add input fields

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,5 @@
 <script>
-
+  import Details from "./components/Details.svelte";
 </script>
 
 <style>
@@ -13,44 +13,7 @@
   <div class="container">
     <div class="row mt-5">
       <div class="col-md-6">
-        <div class="card p-2 shadow">
-          <div class="card-body">
-            <h5 class="card-title mb-4">Add New Note</h5>
-            <form>
-              <div class="form-group">
-                <label for="title">Title</label>
-                <input
-                  type="text"
-                  class="form-control"
-                  id="text"
-                  placeholder="Note Title" />
-              </div>
-              <div class="form-group">
-                <label for="category">Category</label>
-                <select class="form-control" id="category">
-                  <option selected disaabled>Selecet a category</option>
-                  <option>School</option>
-                  <option>School</option>
-                  <option>School</option>
-                </select>
-              </div>
-              <div class="form-group">
-                <label for="content">Content</label>
-                <textarea
-                  class="form-control"
-                  id="content"
-                  rows="3"
-                  placeholder="Note Content" />
-              </div>
-              <button
-                type="submit"
-                on:click|preventDefault={addNote}
-                class="btn btn-primary">
-                Add Note
-              </button>
-            </form>
-          </div>
-        </div>
+        <Details />
       </div>
       <div class="col-md-6">
         <div class="card">
---
+++ src/components/Details.svelte
@@ -0,0 +1,73 @@
+<script>
+  let weight;
+  let height;
+  let age;
+  let gender = "m";
+
+  function submitBMI() {
+    console.log(event);
+    console.log(weight, height, age, gender);
+  }
+</script>
+
+<div class="card p-2 shadow">
+  <div class="card-body">
+    <h5 class="card-title mb-4">Your details</h5>
+    <form>
+      <div class="form-group">
+        <label for="weight">Weight</label>
+        <input
+          type="text"
+          class="form-control"
+          id="weight"
+          placeholder="weight"
+          bind:value={weight} />
+      </div>
+      <div class="form-group">
+        <label for="height">Height</label>
+        <input
+          type="text"
+          class="form-control"
+          id="height"
+          placeholder="height"
+          bind:value={height} />
+      </div>
+      <div class="form-group">
+        <label for="age">Age</label>
+        <input
+          type="text"
+          class="form-control"
+          id="age"
+          placeholder="age"
+          bind:value={age} />
+      </div>
+      <div class="form-check form-check-inline">
+        <input
+          class="form-check-input"
+          type="radio"
+          name="gender"
+          id="male"
+          value="m"
+          bind:group={gender} />
+        <label class="form-check-label" for="male">Male</label>
+      </div>
+      <div class="form-check form-check-inline">
+        <input
+          class="form-check-input"
+          type="radio"
+          name="gender"
+          id="female"
+          value="f"
+          bind:group={gender} />
+        <label class="form-check-label" for="female">Female</label>
+      </div>
+      <div />
+      <button
+        type="submit"
+        on:click|preventDefault={submitBMI}
+        class="btn btn-primary">
+        Add Note
+      </button>
+    </form>
+  </div>
+</div>
